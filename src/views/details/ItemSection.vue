1cae2dbe67dcce4331d67cbb5e974736dd432203
NonSpicyBurrito <burritoyangster@gmail.com>
2026-01-06 09:02:51 +0800

    Added description to ItemSection

--- src/views/details/ItemSection.vue
+++ src/views/details/ItemSection.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import type { ServerItemSection } from '@sonolus/core'
 import ItemCard from '../../components/cards/ItemCard.vue'
+import RichText from '../../components/RichText.vue'
 import { i18nText } from '../../i18n'
 
 defineProps<{
@@ -11,6 +12,8 @@ defineProps<{
 <template>
     <section class="flex flex-col gap-10 sm:gap-12">
         <h2 class="text-30 font-bold sm:text-36">{{ i18nText(section.title) }}</h2>
+
+        <RichText v-if="section.description" :text="section.description" />
 
         <ItemCard v-for="(item, key) in section.items" :key :type="section.itemType" :item />
     </section>

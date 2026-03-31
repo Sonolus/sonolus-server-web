7ee2831888b01fb67dac7338676c19535f8680a4
NonSpicyBurrito <burritoyangster@gmail.com>
2026-03-31 15:57:02 +0800

    Added title to ItemListView

--- src/views/lists/ItemListView.vue
+++ src/views/lists/ItemListView.vue
@@ -21,7 +21,10 @@ defineOptions(
         error: ({ i18n, props: { type } }) =>
             i18n.clients.customServer[type].list.error(import.meta.env.VITE_TITLE),
 
-        title: ({ i18n, props: { type } }) => i18n.routes.server.lists[type].title,
+        title: ({ i18n, props: { type, data } }) =>
+            data?.title
+                ? (i18n.texts[data.title] ?? data.title)
+                : i18n.routes.server.lists[type].title,
     }),
 )
 
